4ffe417625a6440e313d2ef3f309545c037713d1
ganoo914 <80074285+ganoo914@users.noreply.github.com>
2023-04-14 15:16:41 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//https://velog.io/@sgwon1996/%EB%A9%8B%EC%9F%81%EC%9D%B4%EC%82%AC%EC%9E%90%EC%B2%98%EB%9F%BC-9%EA%B8%B0-Javascript-%EA%B3%84%EC%82%B0%EA%B8%B0-%EB%A7%8C%EB%93%A4%EA%B8%B0
 function pv() {
     const name = document.getElementById('name').value;
     document.getElementById("result").innerText = name;
